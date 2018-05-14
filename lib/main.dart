a64d0e04d56bc6a88ac03a4a145286740bbe2ebb
Pavlos Nicolaou <contact@pavlosnicolaou.org>
2018-05-14 18:03:12 +0100

    setted up flutter project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new RealWorldApp());
+
+class RealWorldApp extends StatefulWidget {
+  @override
+    State<StatefulWidget> createState() {
+      return new RealWorldState();
+    }
+}
+
+class RealWorldState extends State<RealWorldApp> {
+  @override
+    Widget build(BuildContext context) {
+      // TODO: implement build
+      return null;
+    }
+}
