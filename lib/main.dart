c9654cf575935b3a6ce55119997493e6fb15b874
Pavlos Nicolaou <contact@pavlosnicolaou.org>
2018-05-14 18:14:23 +0100

    playing with circular porgress bar

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,30 @@ class RealWorldApp extends StatefulWidget {
 }
 
 class RealWorldState extends State<RealWorldApp> {
+  var _isLoading = true;
+
   @override
     Widget build(BuildContext context) {
       // TODO: implement build
-      return null;
+      return new MaterialApp(
+        home: new Scaffold(
+          appBar: new AppBar(
+            title: new Text("REAL WORLD APP BAR"),
+            actions: <Widget>[
+              new IconButton(icon: new Icon(Icons.refresh),
+              onPressed: () {
+                print("Reloading...");
+                setState(() {
+                  _isLoading = false;
+                });
+              },)
+            ],
+          ),
+          body: new Center(
+            child: _isLoading ? new CircularProgressIndicator() :
+              new Text("Finished Loading..."),
+          ),
+        ),
+      );
     }
 }
