b5caef257517f582fe5e0037c457b91e5b1b5d15
Pavlos Nicolaou <contact@pavlosnicolaou.org>
2018-05-14 18:50:22 +0100

    parsing images from the API and showing them in a ListView

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,14 @@ void main() => runApp(new RealWorldApp());
 
 class RealWorldApp extends StatefulWidget {
   @override
-    State<StatefulWidget> createState() {
-      return new RealWorldState();
-    }
+  State<StatefulWidget> createState() {
+    return new RealWorldState();
+  }
 }
 
 class RealWorldState extends State<RealWorldApp> {
   var _isLoading = true;
-
+  var videos;
 
   _fetchData() async {
     print("Attempting to fetch data from network");
@@ -27,39 +27,73 @@ class RealWorldState extends State<RealWorldApp> {
 
       final map = json.decode(response.body);
       final videosJson = map["videos"];
-      videosJson.forEach((video) {
-        print(video["name"]);
-      });
+
+      // videosJson.forEach((video) {
+      //   print(video["name"]);
+      // });
       setState(() {
         _isLoading = false;
+        this.videos = videosJson;
       });
       // print(map["videos"]);
-    }    
+    }
   }
 
   @override
-    Widget build(BuildContext context) {
-      // TODO: implement build
-      return new MaterialApp(
-        home: new Scaffold(
-          appBar: new AppBar(
-            title: new Text("REAL WORLD APP BAR"),
-            actions: <Widget>[
-              new IconButton(icon: new Icon(Icons.refresh),
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("REAL WORLD APP BAR"),
+          actions: <Widget>[
+            new IconButton(
+              icon: new Icon(Icons.refresh),
               onPressed: () {
                 print("Reloading...");
                 setState(() {
                   _isLoading = true;
                 });
                 _fetchData();
-              },)
-            ],
-          ),
-          body: new Center(
-            child: _isLoading ? new CircularProgressIndicator() :
-              new Text("Finished Loading!!!..."),
-          ),
+              },
+            )
+          ],
         ),
-      );
-    }
+        body: new Center(
+          child: _isLoading
+              ? new CircularProgressIndicator()
+              : new ListView.builder(
+                  itemCount: this.videos != null ? this.videos.length : 0,
+                  itemBuilder: (context, i) {
+                    final video = this.videos[i];
+                    return new Column(
+                      children: <Widget>[
+                        new Container(
+                          padding: new EdgeInsets.all(16.0),
+                          child: new Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              new Image.network(video["imageUrl"]),
+                              new Container(
+                                height: 8.0,
+                              ),
+                              new Text(
+                                video["name"],
+                                style: new TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                        new Divider()
+                      ],
+                    );
+                    // return new Text("Row: $i");
+                  },
+                ),
+        ),
+      ),
+    );
+  }
 }
