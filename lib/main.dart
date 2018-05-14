9c25f894344f22d2631c9d815b802973b603f952
Pavlos Nicolaou <contact@pavlosnicolaou.org>
2018-05-14 22:21:15 +0100

    parsing data in the second list of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,206 @@
 import 'package:flutter/material.dart';
-import 'dart:io';
-import 'dart:convert';
 import 'package:http/http.dart' as http;
-import './views/video_cell.dart';
+import 'dart:convert';
+import './models/course.dart';
+import './views/course_row.dart';
 
+void main() => runApp(new RealWorldStatefulBody());
 
-void main() => runApp(new RealWorldApp());
-
-class RealWorldApp extends StatefulWidget {
+class RealWorldStatefulBody extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return new RealWorldState();
   }
 }
 
-class RealWorldState extends State<RealWorldApp> {
+class RealWorldState extends State<StatefulWidget> {
   var _isLoading = true;
-  var videos;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _fetchData();
+  }
+
+  final videos = new List<Video>();
 
   _fetchData() async {
-    print("Attempting to fetch data from network");
+    videos.clear();
+    final uriString = 'https://api.letsbuildthatapp.com/youtube/home_feed';
 
-    final url = "https://api.letsbuildthatapp.com/youtube/home_feed";
-    final response = await http.get(url);
-
+    final response = await http.get(uriString);
     if (response.statusCode == 200) {
-      print(response.body);
-
-      final map = json.decode(response.body);
-      final videosJson = map["videos"];
-
-      // videosJson.forEach((video) {
-      //   print(video["name"]);
-      // });
+      // print(response.body);
+      final coursesJson = json.decode(response.body);
+      // print(coursesJson);
+      coursesJson["videos"].forEach((videoDict) {
+        final course = new Video(videoDict["id"], videoDict["name"],
+            videoDict["imageUrl"], videoDict["numberOfViews"]);
+        videos.add(course);
+      });
       setState(() {
         _isLoading = false;
-        this.videos = videosJson;
       });
-      // print(map["videos"]);
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return new MaterialApp(
+      title: "A Real World App",
       home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("REAL WORLD APP BAR"),
-          actions: <Widget>[
-            new IconButton(
-              icon: new Icon(Icons.refresh),
-              onPressed: () {
-                print("Reloading...");
-                setState(() {
-                  _isLoading = true;
-                });
-                _fetchData();
-              },
-            )
-          ],
-        ),
-        body: new Center(
+          appBar: new AppBar(
+            title: new Text("Real World App Bar"),
+            actions: <Widget>[
+              new IconButton(
+                icon: new Icon(Icons.refresh),
+                color: Colors.white,
+                onPressed: () {
+                  setState(() {
+                    _isLoading = true;
+                    _fetchData();
+                  });
+                },
+              )
+            ],
+          ),
+          // body: new Text("HELLLLLLLOOOOOO THERE")
+          body: new Center(
+              child: _isLoading
+                  ? new CircularProgressIndicator()
+                  : new ListView.builder(
+                      itemCount: videos.length,
+                      itemBuilder: (context, i) {
+                        final video = videos[i];
+                        // return new Text("STUFF");
+                        return new FlatButton(
+                          padding: new EdgeInsets.all(0.0),
+                          child: new CourseRow(video),
+                          onPressed: () {
+                            print("Pressed $i");
+                            Navigator.push(
+                                context,
+                                new MaterialPageRoute(
+                                    builder: (context) =>
+                                        new CourseDetailsPage(video)));
+                          },
+                        );
+                      },
+                    ))),
+    );
+  }
+}
+
+class CourseDetailsPage extends StatefulWidget {
+  final Video video;
+  CourseDetailsPage(this.video);
+  @override
+  State<StatefulWidget> createState() {
+    return new CourseDetailsState(video);
+  }
+}
+
+class Lesson {
+  final String name;
+  final String imageUrl;
+  final String duration;
+  final int number;
+  Lesson(this.name, this.imageUrl, this.duration, this.number);
+}
+
+class CourseDetailsState extends State<CourseDetailsPage> {
+  final Video video;
+  CourseDetailsState(this.video);
+  final lessons = new List<Lesson>();
+
+  var _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchLessons();
+  }
+
+  _fetchLessons() async {
+    final urlString =
+        'https://api.letsbuildthatapp.com/youtube/course_detail?id=' +
+            video.id.toString();
+    print("Fetching: " + urlString);
+    final response = await http.get(urlString);
+    final lessonsJson = json.decode(response.body);
+    lessonsJson.forEach((lessonJson) {
+      final lesson = new Lesson(lessonJson["name"], lessonJson["imageUrl"],
+          lessonJson["duration"], lessonJson["number"]);
+      lessons.add(lesson);
+    });
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(video.name),
+      ),
+      body: new Center(
           child: _isLoading
               ? new CircularProgressIndicator()
               : new ListView.builder(
-                  itemCount: this.videos != null ? this.videos.length : 0,
+                  itemCount: lessons.length,
                   itemBuilder: (context, i) {
-                    final video = this.videos[i];
-                    return new VideoCell(video);
+                    final lesson = lessons[i];
+                    return new Column(
+                      children: <Widget>[
+                        new Container(
+                          padding: new EdgeInsets.all(12.0),
+                          child: new Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              new Image.network(
+                                lesson.imageUrl,
+                                width: 150.0,
+                              ),
+                              new Container(
+                                width: 12.0,
+                              ),
+                              new Flexible(
+                                child: new Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    new Text(
+                                      lesson.name,
+                                      style: new TextStyle(fontSize: 16.0),
+                                    ),
+                                    new Container(
+                                      height: 4.0,
+                                    ),
+                                    new Text(
+                                      lesson.duration,
+                                      style: new TextStyle(
+                                          fontStyle: FontStyle.italic),
+                                    ),
+                                    new Container(
+                                      height: 4.0,
+                                    ),
+                                    new Text(
+                                      "Episode #" + lesson.number.toString(),
+                                      style: new TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        new Divider()
+                      ],
+                    );
                   },
-                ),
-        ),
-      ),
+                )),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:real_world_app_lbta/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
+    await tester.pumpWidget(new RealWorldStatefulBody());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
